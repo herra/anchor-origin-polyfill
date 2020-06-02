909e1b3d0db4c153ea0d1b302f08be3000ad7912
Dimitar Nikolov <d.nikolov@kodar.net>
2020-06-02 11:39:00 +0300

    Fix error with chromium browsers

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-if (typeof HTMLAnchorElement.prototype.origin === 'undefined') {
+if (!('origin' in HTMLAnchorElement.prototype)) {
   Object.defineProperty(HTMLAnchorElement.prototype, 'origin', {
     get: function () {
       return this.protocol + '//' + this.hostname + (this.port ? ':' + this.port : '')
